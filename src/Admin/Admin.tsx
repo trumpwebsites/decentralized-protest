175f0bec93283622ef03c8b51c5ea67586fc53a3
Brandon In <brandonin@me.com>
2020-06-03 13:00:32 -0700

    WIP adding netlify lambda

--- src/Admin/Admin.tsx
+++ src/Admin/Admin.tsx
@@ -3,7 +3,38 @@ import Card from 'react-bootstrap/Card';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 
+import { IPost } from '../Posts/Post';
+
 const Admin: React.FC = () => {
+    // Function using fetch to POST to our API endpoint
+    const onCreatePostClick = () => {
+        console.log("helllo")
+        function createPost(data) {
+            return fetch('/.netlify/functions/post-create', {
+            body: JSON.stringify(data),
+            method: 'POST'
+            }).then(response => {
+                console.log(response)
+            // return response.json()
+            })
+        }
+        
+        // Post data
+        const myPost: IPost = {
+            user: "Brandon In",
+            message: "The Revolution Will Not Be Televised.",
+            time: 1591158839000,
+        }
+        
+        // create it!
+        createPost(myPost).then((response) => {
+            console.log('API response', response)
+            // set app state
+        }).catch((error) => {
+            console.log('API error', error)
+        })
+    }
+
     return (
         <div>
             <Card bg="dark" text="white" style={{ marginTop: "5%" }}>
@@ -13,7 +44,7 @@ const Admin: React.FC = () => {
                             <Form.Group controlId="post">
                                 <Form.Control as="textarea" size="lg" rows={5} placeholder="What's happening?" />
                             </Form.Group>
-                            <Button variant="light" type="submit" size="lg" block>
+                            <Button variant="light" size="lg" onClick={onCreatePostClick} block>
                                 Submit
                             </Button>
                         </Form>
