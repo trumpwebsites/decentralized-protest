9bd452bd3cd5a95c9714d7ee86b65cb7a5c03de2
Brandon In <brandonin@me.com>
2020-06-03 00:57:20 -0700

    grab correct url for qr scanner

--- src/QRCode/QRCode.tsx
+++ src/QRCode/QRCode.tsx
@@ -1,14 +1,11 @@
 import React from "react";
 import QRCodeReact from "qrcode.react";
 
-const QRCode: React.FC = () => {
-    console.log(process.env.URL)
-    return (
-        <QRCodeReact 
-            value={`${process.env.URL}`} 
-            size={300}
-        />
-    )
-}
+const QRCode: React.FC = () => (
+    <QRCodeReact 
+        value={window.location.href} 
+        size={300}
+    />
+)
 
 export default QRCode;
