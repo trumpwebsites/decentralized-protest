675ccc1e9bcddeb90435a1fd04d0c67c21a3f359
Brandon In <brandonin@me.com>
2020-06-02 23:25:18 -0700

    moved styling to .css file

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,18 +1,21 @@
 import React, { lazy } from "react";
-import Navbar from 'react-bootstrap/Navbar';
-import { BsBoxArrowUp } from 'react-icons/bs';
+import Navbar from "react-bootstrap/Navbar";
+import { BsBoxArrowUp } from "react-icons/bs";
+import "./Home.css";
 
 const Posts = lazy(() => import('./Posts'));
 
 const Home: React.FC = () => {
     return (
         <div>
-            <Navbar bg="dark" sticky="top" variant="dark" className="justify-content-center" style={{ borderBottom: "1px solid #282c34" }}>
+            <Navbar bg="dark" sticky="top" variant="dark" className="justify-content-center navbar-border" style={{ borderBottom: "1px solid #282c34" }}>
                 <Navbar.Brand href="#">
                     {process.env.REACT_APP_APPLICATION_NAME}
                 </Navbar.Brand>
             </Navbar>
-            <button type="button" className="btn btn-light btn-circle btn-xl" style={{ position: "fixed", right: 0, bottom: 0, zIndex: 9999 }}><BsBoxArrowUp width="40px" height="40px" /></button>
+            <button type="button" className="btn btn-light btn-circle btn-xl fixed-button">
+                <BsBoxArrowUp width="40px" height="40px" />
+            </button>
             <div style={{margin: "5%"}}>
                 <Posts />
             </div>
